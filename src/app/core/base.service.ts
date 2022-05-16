ae98cccd19c910d8312b2c551f2dc3a4605a32b6
sergeylihodeevzelenyuk <sergeylihoddev@gmail.com>
2022-05-16 14:27:02 +0300

    bease service codeStyle

--- src/app/core/base.service.ts
+++ src/app/core/base.service.ts
@@ -17,58 +17,52 @@ export abstract class BaseService<
   }
 
   public fetchAll(): Observable<T[]> {
-    let url = this.url.href;
-
     if ((this.serverName = environment.SERVERS_NAME.FIREBASE)) {
-      url = `${this.url.href}.json`;
-
-      return this.http.get<T[]>(url).pipe(map(this.firebaseDataModifier));
+      return this.http
+        .get<T[]>(`${this.url.href}.json`)
+        .pipe(map(this.firebaseDataModifier));
     }
 
-    return this.http.get<T[]>(url);
+    return this.http.get<T[]>(this.url.href);
   }
 
   public fetch(id: string): Observable<T> {
-    let url = this.url.href;
-
     if ((this.serverName = environment.SERVERS_NAME.FIREBASE)) {
-      url = `${this.url.href}/${id}.json`;
-
       return this.http
-        .get<T>(url)
+        .get<T>(`${this.url.href}/${id}.json`)
         .pipe(map((fetchedItem) => ({ ...fetchedItem, id })));
     }
 
-    return this.http.get<T>(url);
+    return this.http.get<T>(this.url.href);
   }
 
   public add(item: T): Observable<{ [key: string]: string }> {
-    let url = this.url.href;
-
     if ((this.serverName = environment.SERVERS_NAME.FIREBASE)) {
-      url = `${this.url.href}.json`;
+      return this.http.post<{ [key: string]: string }>(
+        `${this.url.href}.json`,
+        item
+      );
     }
 
-    return this.http.post<{ [key: string]: string }>(url, item);
+    return this.http.post<{ [key: string]: string }>(this.url.href, item);
   }
 
   public edit(item: T, id: string): Observable<any> {
-    let url = this.url.href;
-
     if ((this.serverName = environment.SERVERS_NAME.FIREBASE)) {
-      url = `${this.url}/${id}.json`;
+      return this.http.put<{ [key: string]: string }>(
+        `${this.url}/${id}.json`,
+        item
+      );
     }
 
-    return this.http.put<{ [key: string]: string }>(url, item);
+    return this.http.put<{ [key: string]: string }>(this.url.href, item);
   }
 
   public delete(id: string): Observable<any> {
-    let url = this.url.href;
-
     if ((this.serverName = environment.SERVERS_NAME.FIREBASE)) {
-      url = `${this.url.href}/${id}.json`;
+      return this.http.delete<void>(`${this.url.href}/${id}.json`);
     }
 
-    return this.http.delete<void>(url);
+    return this.http.delete<void>(this.url.href);
   }
 }
